96cc598cf867ceaf27aa16f836a5ca2486673f3a
Don Koteyka <don.kotejka@yandex.ru>
2023-04-28 20:00:06 +0300

    second commit

--- VK_parser.py
+++ VK_parser.py
@@ -1,5 +1,43 @@
 import requests
+import json
+import os.path
+import pandas as pd
+from pprint import pprint
+
+def write_json(dict):
+    if os.path.exists('photo_vk.json'):
+        with open('photo_vk.json', 'w', encoding='utf-8') as f:
+            json.dump(dict, f)
+    else:
+        with open('photo_vk.json', 'x', encoding='utf-8') as f:
+            json.dump(dict, f)
 class Vk_pars:
     def __init__(self, token):
         self.token = token
+    def get_photo_link(self, vk_id, ):
+        url = 'https://api.vk.com/method/photos.getAll'
+        param = {
+            'owner_id' : vk_id,
+            'access_token':self.token,
+            'extended':'1',
+            'offset':'0',
+            'v':'5.131'
+
+        }
+        res = requests.get(url, params=param)
+        return res.json()
+
+
+if __name__ == '__main__':
+    with open('private/token.txt', 'rt', encoding='utf-8') as f:
+        ya_token = f.readline()
+        vk_token = f.readline()
+        vk = Vk_pars(vk_token)
+        vk_id = '10505481'
+        res = vk.get_photo_link(vk_id)
+        pprint(res)
+        write_json(res)
+
+
+
 
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-import ya_api as ya
-import VK_parser as vk
+from ya_api import *
+from VK_parser import *
 import time
 from progress.bar import Bar
 bar = Bar('Countdown', max = 100)
 
 if __name__ == '__main__':
-    with open('token.txt', 'rt', encoding='utf-8') as f:
+    with open('private/token.txt', 'rt', encoding='utf-8') as f:
         ya_token = f.readline()
         vk_token = f.readline()
+    # owner_id = input('Ведите id пользователя:')
+    ya = YandexDisk(ya_token)
+    vk = Vk_pars(vk_token)
+
+
+
+
+
+
 
 
 
