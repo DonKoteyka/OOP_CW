24d7d9c64c553048c958338d51c33d515b9b0e6c
Don Koteyka <don.kotejka@yandex.ru>
2023-05-01 17:03:08 +0300

    6th commit

--- VK_parser.py
+++ VK_parser.py
@@ -1,22 +1,9 @@
 import requests
 import json
 import os.path
+import time
 # import pandas as pd
 from pprint import pprint
-
-
-# def write_json(dict):
-#     if os.path.exists('photo_vk.json'):
-#         with open('photo_vk.json', 'w', encoding='utf-8') as f:
-#             json.dump(dict, f)
-#     else:
-#         with open('photo_vk.json', 'x', encoding='utf-8') as f:
-#             json.dump(dict, f)
-#
-# def write_vk_img(url_img, file_name):
-#     response = requests.get(url_img)
-#     with open(f'{file_name}.jpg', 'wb') as f:
-#         f.write(response.content)
 
 class Vk_pars:
     url = 'https://api.vk.com/method/'
@@ -28,7 +15,7 @@ class Vk_pars:
         res_total = list()
         offset = int()
         while True:
-            if offset == 1000:
+            if offset == 3000:
                 break
             param = {
                 'owner_id' : vk_id,
@@ -41,8 +28,9 @@ class Vk_pars:
             res = requests.get(url_get_photo, params=param).json()['response']['items']
             res_total += res
             offset += 20
+            time.sleep(0.33)
         return res_total
-    def get_json_vk(self, vk_id):
+    def get_json_img(self, vk_id):
         vk_dic = self.get_photo_link(vk_id)
         j_list, val_1, val_2, link = list(), str(), int(), list()
         for i in vk_dic:
@@ -56,33 +44,20 @@ class Vk_pars:
                 "size": i['sizes'][val_2]['type']
             }
             j_list.append(j_dic)
-            return j_list, link
-    def write_json_vk(self, vk_id, json_name = 'photo_vk.json'):
-        vk_dic = self.get_photo_link(vk_id)
-        j_list, val_1, val_2, link = list(), str(), int(), list()
-        for i in vk_dic:
-            for index, size_dict in enumerate(i['sizes']):
-                if size_dict['type'] > val_1:
-                    val_1 = size_dict['type']
-                    val_2 = index
-            link.append(i['sizes'][val_2]['url'])
-            j_dic = {
-                "file_name": i['id'],
-                "size": i['sizes'][val_2]['type']
-            }
-            j_list.append(j_dic)
+        return j_list, link
+    def write_json_img(self, vk_id, json_name = 'photo_vk.json'):
+        j_list = self.get_json_img(vk_id)[0]
+
         if os.path.exists(f'{json_name}'):
             with open(f'{json_name}', 'w', encoding='utf-8') as f:
                 json.dump(j_list, f)
         else:
             with open(f'{json_name}', 'x', encoding='utf-8') as f:
                 json.dump(j_list, f)
-        return link
-    def get_photo_img(self, vk_id, json_name = 'photo_vk.json', dir = 'photo'):
-        list_links = self.write_json_vk(vk_id)
-        with open(json_name, 'r', encoding='utf-8') as f:
-            list_j = json.load(f)
-        list_names = [x['file_name'] for x in list_j]
+
+    def get_photo_img(self, vk_id, dir = 'photo'):
+        list_links = self.get_json_img(vk_id)[0]
+        list_names = self.get_json_img(vk_id)[1]
         for link, file_name in zip(list_links, list_names):
             response = requests.get(link)
             with open(f'{dir}/{file_name}.jpg', 'wb') as f:
@@ -95,8 +70,9 @@ if __name__ == '__main__':
         vk_token = f.readline()
     vk = Vk_pars(vk_token)
     vk_id = '52082161'
-    # res = vk.write_json_vk(vk_id)
+    # res = vk.write_json_img(vk_id)
     res = vk.get_photo_link(vk_id)
+    # res = vk.get_json_img(vk_id)
     # vk.get_photo_img(vk_id)
     # print(pd.DataFrame(res))
     pprint(res)
