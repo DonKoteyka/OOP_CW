c9bf95327a2e62e06902d8e8ce61ce1bc7b024f9
don_koteyka_mac <777zik@gmail.com>
2023-05-09 19:19:47 +0300

    13th commit

--- main.py
+++ main.py
@@ -15,16 +15,18 @@ logging.info('Hello')
 
 if __name__ == '__main__':
     vk_id = int(input('Введите ID:'))
-    # vk_id = '10505481'
-    with open('private/token.txt', 'rt', encoding='utf-8') as f:
-        ya_token = f.readline().strip('\n')
-        vk_token = f.readline().strip('\n')
+    # ya_token = input('Введите токен от яндекс диска:')
+    # vk_id = '1'
+    # with open('private/token.txt', 'rt', encoding='utf-8') as f:
+    #     ya_token = f.readline().strip('\n')
+    #     vk_token = f.readline().strip('\n')
     ya = YandexDisk(ya_token)
     vk = Vk_Avatar(vk_token)
-    dict = vk.get_dict_img(vk_id, 10)
+    dict = vk.get_dict_img(vk_id, 5)
+    # vk.write_img_json(vk_id)
     bar = Bar('Processing', max=len(dict))
     for j in dict:
-        ya.upload_link_to_disk(str(j['file_name']), str(j['url']))
+        ya.upload_link_to_disk(j['file_name'], j['url'])
         bar.next()
     bar.finish()
 
